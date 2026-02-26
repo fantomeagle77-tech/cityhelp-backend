ed3edca15d6b7ce7c61f95e0d3deffb564bf5f6b
fantomeagle77-tech <fantomeagle77@gmail.com>
2026-02-26 13:35:42 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,6 +22,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://cityhelp-frontend.pages.dev",
+        "https://cityhelp.app",
+        "https://www.cityhelp.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
